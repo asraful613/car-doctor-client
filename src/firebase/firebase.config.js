10ef964b6128f9c8ca01b0f9d3923b6779de64ee
asraful613 <atifasraful530@gmail.com>
2024-05-04 13:08:36 +0100

    added a firebase config key and hide all

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,16 +1,13 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCFwUOgaebD5pu23Fbauo-wzClWcYkfxQQ",
-  authDomain: "cars-doctor-55760.firebaseapp.com",
-  projectId: "cars-doctor-55760",
-  storageBucket: "cars-doctor-55760.appspot.com",
-  messagingSenderId: "421167269148",
-  appId: "1:421167269148:web:649516bdc8f50edd38a6d9"
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID
 };
 
 // Initialize Firebase
