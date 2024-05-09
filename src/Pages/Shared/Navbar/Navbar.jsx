005e29574e12c12f7864d1d22d3d7c5111f0c46b
asraful613 <atifasraful530@gmail.com>
2024-05-09 11:39:48 +0100

    added a my booking list

--- src/Pages/Shared/Navbar/Navbar.jsx
+++ src/Pages/Shared/Navbar/Navbar.jsx
@@ -13,7 +13,10 @@ const Navbar = () => {
                   <li><Link to={'/'}>Home</Link></li>
                   <li><Link to={'/about'}>About</Link></li>
                   {
-                    user?.email?<li><button onClick={handleLogOut}>LogOut</button></li>
+                    user?.email?<>
+                    <li><Link to={'/bookings'}>My Bookings</Link></li>
+                    <li><button onClick={handleLogOut}>LogOut</button></li>
+                    </>
                     :
                     <li><Link to={'/login'}>Login</Link></li>
                   }
--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,6 +4,8 @@ import Home from "../Pages/Home/Home/Home";
 import Login from "../Pages/Login/Login";
 import SignUp from "../Pages/SignUp/SignUp";
 import CheckedOut from "../Pages/CheckedOut/CheckedOut";
+import Bookings from "../Pages/Bookings/Bookings";
+import PrivateRoute from "./PrivateRoute";
 
 const router = createBrowserRouter([
     {
@@ -26,7 +28,11 @@ const router = createBrowserRouter([
           path:"checkedOut/:id",
           element:<CheckedOut></CheckedOut>,
           loader:({params})=>fetch(`http://localhost:5000/services/${params.id}`)
-        }
+        },
+        {
+          path:'bookings',
+          element:<PrivateRoute><Bookings></Bookings></PrivateRoute>
+        },
       ]
     },
   ]);
